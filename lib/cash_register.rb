683c1762ecfc93c8b3424011ef5087953e539ddd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-01 18:56:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,22 +9,20 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    @total+=(price*quantity)
-    @last_transaction = price*quantity
+    self.total+=(price*quantity)
+    self.last_transaction = price*quantity
     quantity.times do
       items << title
     end
   end
 
   def apply_discount
-    @total*=((100-@discount)*0.01)
-    @total = @total.round
-    @discount != 0 ? "After the discount, the total comes to $#{@total}." : "There is no discount to apply."
+    self.total*=((100-self.discount)*0.01)
+    self.total = self.round
+    self.discount != 0 ? "After the discount, the total comes to $#{self.total}." : "There is no discount to apply."
   end
 
-
-
   def void_last_transaction
-    @total-= @last_transaction
+    self.total-= self.last_transaction
   end
 end
